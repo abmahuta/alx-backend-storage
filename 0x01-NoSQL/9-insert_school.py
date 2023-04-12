10549a64ddd87e3a515ca1d4ce05be1fbbc0f175
Abmahuta <mahuta2012@gmail.com>
2023-04-12 11:36:04 +0100

    error fix

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -7,4 +7,4 @@ def insert_school(mongo_collection, **kwargs):
     '''Inserts a new document in a collection.
     '''
     result = mongo_collection.insert_one(kwargs)
-     return result.inserted_id
+    return result.inserted_id
